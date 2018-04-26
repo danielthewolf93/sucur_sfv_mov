cd7dcdfdab18ec28eeddb9914f795849a51dea8a
Daniel White <danieltesorero2009@gmail.com>
2018-04-26 12:51:12 -0300

    falta_controlar_entre_fechas
    ++imprimir_y/o_exportar_excel

--- controladores/buscar_mov_cuit.php
+++ controladores/buscar_mov_cuit.php
@@ -1,6 +1,35 @@
 <?php
 
 
+$cuit=$_GET['cuit_cont'];
+
+$mysqli = new mysqli("localhost", "root", "", "rentascf");
+
+/* comprobar la conexión */
+if (mysqli_connect_errno()) {
+    printf("Falló la conexión: %s\n", mysqli_connect_error());
+    exit();
+}
+
+
+
+$consulta3 = "SELECT * FROM rm_sucursales_mov WHERE cuit_contrib=$cuit ";
+
+
+//Funciona sola
+$resultado = $mysqli->query($consulta3);
+
+
+
+require '../vistas/vista_suc_tramites_mov_cuit.html';
+
+
+
+
+
+
+
+
 
 
 ?>
--- controladores/buscar_mov_fecha.php
+++ controladores/buscar_mov_fecha.php
@@ -1,6 +1,50 @@
 <?php
 
 
+date_default_timezone_set('America/Argentina/Catamarca');
+
+
+
+
+
+$fechadesde=$_GET['fecha_desde'];
+$fechahasta=$_GET['fecha_hasta'];
+
+
+
+
+$fecha1=date( "Y-m-d", strtotime( $fechadesde ) );
+
+$fecha2=date( "Y-m-d", strtotime( $fechahasta ) );
+
+
+
+
+$mysqli = new mysqli("localhost", "root", "", "rentascf");
+
+/* comprobar la conexión */
+if (mysqli_connect_errno()) {
+    printf("Falló la conexión: %s\n", mysqli_connect_error());
+    exit();
+}
+
+
+
+$consulta3 = "SELECT * FROM rm_sucursales_mov  WHERE fecha_tramite  BETWEEN $fecha1 AND $fecha2 ";
+
+
+//Funciona sola
+
+$resultado = $mysqli->query($consulta3);
+
+
+
+require '../vistas/vista_suc_tramites_mov_fecha.html';
+
+
+
+
+
 
 
 
