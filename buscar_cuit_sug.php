08c3c725839ae32d0e0cf87c0a4944add62cd434
Daniel White <danieltesorero2009@gmail.com>
2018-04-27 10:40:26 -0300

    funcionando_pred_x_cuit

---
+++ buscar_cuit_sug.php
@@ -0,0 +1,52 @@
+<?php 
+
+
+
+
+
+//Guiahttps://blog.reaccionestudio.com/busqueda-instantanea-con-jquery-y-php/
+
+      $buscar = $_POST['b'];
+       
+      if(!empty($buscar)) {
+            buscar($buscar);
+      }
+       
+      function buscar($b) {
+
+      		$mysqli = new mysqli("localhost", "root", "", "rentascf");
+
+			/* comprobar la conexión */
+			if (mysqli_connect_errno()) {
+			    printf("Falló la conexión: %s\n", mysqli_connect_error());
+			    exit();
+				}
+
+
+
+				$query = "SELECT * FROM rm_padron6 WHERE pad_ente=20 AND pad_cuit_index LIKE '%".$b."%'";
+				$result = $mysqli->query($query);
+
+				$numfilas = $result->num_rows;
+
+
+
+				if($numfilas == 0){
+                  echo "No se han encontrado resultados para '<b>".$b."</b>'.";
+		            }else{	
+		            	//echo "Sugerencias"."</br>";
+		                  foreach ($result as $res ) {
+		                  	
+		                        $nombre = $res['pad_nombre_propietario'];
+		                        $id = $res['pad_cuit_index'];
+		                         
+		                        echo $nombre." ".$id."<br /><br />";    
+		                  									}
+		            	}
+		  }
+
+
+
+
+
+?>
